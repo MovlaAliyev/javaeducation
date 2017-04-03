2ca314f1ebb6d8c0b043e96a13ad04333b691075
movla_aliyev <eliyevmovlan@gmail.com>
2017-04-03 11:49:22 +0400

    Add files via upload

---
+++ Encapsulation/src/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+
+public class Encapsulation {
+
+    public static void main(String[] args) {
+        TestEnc testEnc = new TestEnc();
+        
+        testEnc.setValOne(1);
+        
+        System.out.println(testEnc.getValOne());
+    }
+    
+}
---
+++ Encapsulation/src/encapsulation/TestEnc.java
@@ -0,0 +1,23 @@
+package encapsulation;
+
+public class TestEnc {
+    private int valOne;
+    private int valTwo;
+    
+    public void setValOne(int valOne){
+        this.valOne = valOne;
+    }
+    
+    public void setValTwo(int valTwo){
+        this.valTwo = valTwo;
+    }
+    
+    public int getValOne(){
+        return valOne;
+    }
+    
+    public int getValTwo(){
+        return valTwo;
+    }
+}
+
